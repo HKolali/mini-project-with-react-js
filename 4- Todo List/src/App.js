b98e0757fb9320a15d7a6c2ad624debd1012088d
Hossein Kolali <149469159+HKolali@users.noreply.github.com>
2024-01-13 17:06:07 +0330

    Add files via upload

---
+++ 4- Todo List/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Component/Header/Header'
+import Todo from './Component/Todo/Todo'
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
